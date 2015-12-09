468ccafff5a3ec7f31a5bc07253b229b35147d3b
brurubio <bru.rubio@hotmail.com>
2015-12-09 11:03:27 -0200

    Adicionar novas páginas
    Criação da página de Salas
    e redirecionamento para criação de nova Sala/Entrar em nova Sala

---
+++ newclass.php
@@ -0,0 +1,115 @@
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Paideia - Sistema de educação</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="http://www.formmail-maker.com/var/demo/jquery-popup-form/colorbox.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">Padeia</a></h1>
+				<!--		<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="index.html">Home</a></li>
+											<li><a href="generic.html">Generic</a></li>
+											<li><a href="elements.html">Elements</a></li>
+											<li><a href="#">Sign Up</a></li>
+											<li><a href="#">Log In</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav> -->
+					</header>
+
+
+						<section class="wrapper style5">
+							<div class="inner">
+
+								<section>
+									<center><h4>Nova Sala</h4></center>
+								<!-- </br> -->
+								<!-- Se for professor casatra nova sala -->
+									<form name ="newClassCad" method="post" action="#">
+										<div class="">
+										<center>
+										</br>
+											<div class="6u$ 12u$(xsmall)">
+												<input type="text" name="class-name" id="class-name" value="" placeholder="Nome da Classe" />
+											</div>
+										</br>
+											<div class="6u$ 12u$(xsmall)">
+												<!-- echo php mostra código sala -->
+											</div>
+										</br>
+										</div>
+										</br>
+											<div class="12u$">
+												<center>
+												<ul class="actions">
+													<li><input type="submit" value="Criar Sala" class="special" /></li>
+												</ul>
+												</center>
+											</div>
+										</center>
+										</div>
+									</form>
+							<!-- Se for aluno, entra em nova sala -->
+								<!-- 	<form name ="enterClass" method="post" action="#">
+										<div class="">
+										<center>
+										</br>
+											<div class="6u$ 12u$(xsmall)">
+												<input type="text" name="class-Cod" id="class-Cod" value="" placeholder="Código da Sala" />
+											</div>
+										</br>
+										/br>
+											<div class="12u$">
+												<center>
+												<ul class="actions">
+													<li><input type="submit" value="Entrar Sala" class="special" /></li>
+												</ul>
+											</center>
+											</div>
+										</center>
+										</div>
+									</form> -->
+								</section>
+
+							</div>
+						</section>
+					<!-- </article> -->
+
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
