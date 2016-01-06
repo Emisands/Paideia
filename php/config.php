c2d2299e6d1f231ad2950f30312907eb876fe61f
João Vasconcelos <dev.vasconcelos@gmail.com>
2016-01-06 16:30:16 -0200

    PHP
    Add config.php, register.php - outros php em branco para ser
    implementado.

---
+++ php/config.php
@@ -0,0 +1,4 @@
+<?php
+	$conexao = mysql_connect ("localhost", "user", "senha");
+	mysql_select_db ("padeia");
+?>
---
+++ php/register.php
@@ -0,0 +1,28 @@
+﻿<html>
+<head>
+
+<?php
+  include "config.php";
+
+  $name= $_REQUEST['name'];
+  $email= $_REQUEST['email'];
+  $roomkey= $_REQUEST['roomkey'];
+  $teacher= $_REQUEST['teacher'];
+  
+  $sql = "INSERT INTO register VALUES";
+  $sql .= "('$name', '$email', '$roomkey', '$teacher')";
+  $resultado = mysql_query ($sql);
+
+	
+	mysql_close($conexao);
+ ?>
+<script>
+function CloseMe() 
+{
+    window.close();
+}
+</script>
+</head>
+<body onLoad="CloseMe()">
+</body>
+</html>
