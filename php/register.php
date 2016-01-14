ce0b1c666e675e030f853cebe5590ebfdff197fe
MatheusDeronsi <matheus_naror@hotmail.com>
2016-01-14 17:13:16 -0200

    atualização do cadastro
    cadastro funcionando, mudanças em register.php e register.html

--- php/register.php
+++ php/register.php
@@ -2,20 +2,51 @@
 <head>
 
 <?php
-  include "config.php";
+  $password = $_POST['cad-pass'];
+  $confpass = $_POST['cad-cpass'];
 
-  $name= $_REQUEST['name'];
-  $email= $_REQUEST['email'];
-  $password = $_REQUEST['password'];
-  $roomkey= $_REQUEST['roomkey'];
-  $teacher= $_REQUEST['teacher'];
-  
-  $sql = "INSERT INTO register VALUES";
-  $sql .= "('$name', '$email', '$password', '$roomkey', '$teacher')";
-  $resultado = mysql_query ($sql);
+  if ($password == $confpass) {
+
+    include "config.php";
+
+
+    if (isset($_POST['cad-name'])) {
+      $name = $_POST['cad-name'];
+    }
+    if (isset($_POST['cad-email'])) {
+      $email = $_POST['cad-email'];
+    }
+    if (isset($_POST['cad-pass'])) {
+      $password = $_POST['cad-pass'];
+    }
+    if (isset($_POST['reg-priority'])) {
+      $tipouser = $_POST['reg-priority'];
+    }
+    if (isset($_POST['cad-inst'])) {
+      $instid = $_POST['cad-inst'];
+    }  
+    
+    $consulta = mysql_query("SELECT max(IDuser) FROM account");
+
+    while($row = mysql_fetch_array($consulta)){
+      $newid = $row["max(IDuser)"];
+    }
+
+    if ($newid == NULL)
+      $newid = 0;
+    else ++$newid;
+
+
+    $sql = "INSERT INTO account VALUES";
+    $sql .= "('$email', '$name', '$password','$newid','$tipouser', '$instid')";
+    $resultado = mysql_query ($sql);
 
 	
-	mysql_close($conexao);
+	 mysql_close($conexao);
+   echo "<script> alert('Usuário cadastrado com sucesso!'); history.back(); </script>";
+  }
+
+  else echo "<script> alert('Senhas não conferem'); history.back(); </script>";
 
 /* TA FEITO PARA ABRIR E FECHAR A PAGINA - EM BREVE MUDAR PARA ALERTA */
 
